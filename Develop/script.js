3580e0a76a0b8bb11bf114a12430a918a67863da
Chrissypike <you@example.com>
2023-03-21 12:37:08 -0500

    adding comments

--- Develop/script.js
+++ Develop/script.js
@@ -14,23 +14,10 @@ $(function () {
         console.log('working');
     })
 
-
-
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-
-
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-
   
   });
 
+  // function to upload the data 
   const day = document.getElementById("currentDay");
 
   function updateDay() {
